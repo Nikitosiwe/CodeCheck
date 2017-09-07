0637ee1922a2a40e744e750708413c2519bda32f
Nikitosiwe <nikitos673@gmail.com>
2017-09-07 21:05:30 +0300

    Остановился на удалении комментариев из кода.

--- SplitCode.py
+++ SplitCode.py
@@ -3,12 +3,29 @@ def setFile(pathFile):
         file = open(pathFile,'r')
         codeText = file.read()
         file.close()
+        #return ''.join(codeText.splitlines())
         return codeText
     except FileNotFoundError:
         print("Файл не найден")
 
+def deleteComment(codeText):
+    #for i in range(0,len(codeText)):
+    #    if(codeText[i]=="/" and )
+    commentText = ' string tmp = "asdad//"+"asda//q"; //+"// djjd  '
+
+    for tmp in commentText.split('\n'):
+        if '//' in tmp:
+            print(tmp)
+
+    return codeText
+
+
 def getBlocks(codeText):
-    pass
+    #blocks=[b for b in codeText.replace(' ', ';').replace('(',';').replace(')',';').replace('{',';').replace('}',';').split(';') if b!='']
+    blocks=[]
+    symbols = [' ',';','(',')','{','}']
+    codeText_nonComment = deleteComment(codeText)
+    return blocks
 
 if __name__ == '__main__':
-    print(setFile('TestCode'))
+    print(getBlocks(setFile('TestCode')))
