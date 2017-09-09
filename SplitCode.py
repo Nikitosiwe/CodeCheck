17317d5ca8d64120ba05296c75218a05bd635c09
Nikitosiwe <nikitos673@gmail.com>
2017-09-09 23:17:43 +0300

    Остановился на разбиении кода на блоки по соответствующим классам.

--- SplitCode.py
+++ SplitCode.py
@@ -1,3 +1,5 @@
+from CodeBlocks import *
+
 def setFile(pathFile):
     try:
         file = open(pathFile,'r')
@@ -11,21 +13,58 @@ def setFile(pathFile):
 def deleteComment(codeText):
     #for i in range(0,len(codeText)):
     #    if(codeText[i]=="/" and )
-    commentText = ' string tmp = "asdad//"+"asda//q"; //+"// djjd  '
+    #commentText = ' string tmp = "asdad//"+"asda//q"; //+"// djjd  '
 
-    for tmp in commentText.split('\n'):
-        if '//' in tmp:
-            print(tmp)
+    #for tmp in commentText.split('\n'):
+        #if '//' in tmp:
+            #print(tmp)
 
     return codeText
 
 
 def getBlocks(codeText):
     #blocks=[b for b in codeText.replace(' ', ';').replace('(',';').replace(')',';').replace('{',';').replace('}',';').split(';') if b!='']
-    blocks=[]
-    symbols = [' ',';','(',')','{','}']
-    codeText_nonComment = deleteComment(codeText)
+    blocks=Container()
+    symbols = [';','.','{']
+    blockText = []
+    for ch in codeText:
+        try:
+            index = symbols.index(ch)
+            #print(''.join(blockText)+ch)
+            blockText.append(ch)
+            cont = Container()
+            if index == 0:
+                sem = Semicolon()
+                sem.content.append(blockText)
+                cont.content.append(sem)
+            elif index == 1:
+                pass
+            elif index == 2:
+                pass
+                #fig = Figure()
+                #fig.head.append(blockText)
+                #cont.content.append(fig)
+            #if(len(cont.content)>0):
+                #print(cont.content[0].content)
+            blocks.content.append(cont)
+            #print(blocks.content[0].content[0].content)
+            blockText.clear()
+        except ValueError:
+            blockText.append(ch)
     return blocks
 
+def addToBlock(blocks,block):
+    pass
+
+def printList(lst, tab):
+    for l in lst:
+        if isinstance(l,list):
+            printList(l,tab+'\t')
+        else:
+            print(tab,l)
+
 if __name__ == '__main__':
-    print(getBlocks(setFile('TestCode')))
+    print(getBlocks(setFile('TestCode')).content[0].content[0].content)
+
+
+
